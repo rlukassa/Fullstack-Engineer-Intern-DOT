43db4afbad8bc1f55fa9c7990b2ae88f5c602b65
rlukassa <19623069@std.stei.itb.ac.id>
2026-02-05 03:09:36 +0700

    feat(dot): added partials instead for components folder

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  // Configure Handlebars view engine
   app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
   app.setViewEngine('hbs');
-
-  // Serve static files
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
-  console.log(`✓ Application is running on: http://localhost:${port}`);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
